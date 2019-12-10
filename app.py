e31c96042323cd3dbc39ce3bd3341282e1b3d976
Clark Elijah Whitsett <ewhitsett@Clarks-MacBook-Pro.local>
2019-12-09 17:37:11 -0800

    Done

--- app.py
+++ app.py
@@ -3,31 +3,44 @@ import os
 
 app = Flask(__name__)
 
-
 showcase = []
 others   = []
+order    = []
 def get_them():
-    global showcase, others
-    showcase = ["showcase/"+x for x in os.listdir("static/showcase")]
-    others   = ["art/"+x for x in os.listdir("static/art")]
+    global showcase, others,order
+    base = ''
+    showcase = ["showcase/"+x for x in os.listdir(base+"static/showcase")]
+    others   = ["art/"+x for x in os.listdir(base+"static/art")]
 
-    them = []
-    for i in range(0,len(others),4):
-        them.append(others[i:i+4])
+    them = {}
+    for f in others:
+        path = os.path.join(base+"static/", f)
+        if os.path.isdir(path):
+            folder = "".join(f.split("/")[-1])
+            them[folder] = []
+            order.append(folder)
+            for subfile in os.listdir(path):
+                newpath = os.path.join(f+"/",subfile)
+                if not os.path.isdir(newpath):
+                    if subfile.endswith(".txt"):
+                        continue
+                    img = os.path.join("",newpath)
+                    title = ""
+                    desc = ""
+                    name = "".join(subfile.split(".")[:-1])
+                    if os.path.exists(os.path.join(path,name+".txt")):
+                        d_file = open(os.path.join(path,name+".txt"),'r')
+                        title  = d_file.readline().strip()
+                        desc   = d_file.read().strip().replace("\n", " ")
+                    print(img)
+                    them[folder].append({"img":img, "title":title, "desc":desc}) 
 
     others   = them
 
 @app.route("/")
 def index():
-    return render_template("home.html",big=showcase[0],showcase=showcase[1:], others=others)
-
-@app.route("/home")
-def home():
-    return render_template("index.html",big=showcase[0],showcase=showcase[1:], others=others)
-
-@app.route("/home1")
-def homer():
-    return render_template("index2.html",big=showcase[0],showcase=showcase[1:], others=others)
+    print(others)
+    return render_template("index.html",big=showcase[0],showcase=showcase[1:], others=others, order=order)
 
 get_them()
 if __name__ == "__main__":
