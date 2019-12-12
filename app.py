0241c9867ba0166cc372b280817064164b906367
Clark Elijah Whitsett <ewhitsett@Clarks-MacBook-Pro.local>
2019-12-11 18:51:19 -0800

    Am I done?

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__)
 
 showcase = []
 others   = []
-order    = []
+data     = {}
+bio      = []
 def get_them():
-    global showcase, others,order
+    global showcase, others,data
     base = ''
     showcase = ["showcase/"+x for x in os.listdir(base+"static/showcase")]
     others   = ["art/"+x for x in os.listdir(base+"static/art")]
@@ -18,7 +19,6 @@ def get_them():
         if os.path.isdir(path):
             folder = "".join(f.split("/")[-1])
             them[folder] = []
-            order.append(folder)
             for subfile in os.listdir(path):
                 newpath = os.path.join(f+"/",subfile)
                 if not os.path.isdir(newpath):
@@ -29,18 +29,28 @@ def get_them():
                     desc = ""
                     name = "".join(subfile.split(".")[:-1])
                     if os.path.exists(os.path.join(path,name+".txt")):
+                        print(name)
                         d_file = open(os.path.join(path,name+".txt"),'r')
                         title  = d_file.readline().strip()
                         desc   = d_file.read().strip().replace("\n", " ")
-                    print(img)
-                    them[folder].append({"img":img, "title":title, "desc":desc}) 
+                        d_file.close()
+                        print("TIT", title)
+                        print("DSC", desc)
+                    grouped = {"img":img, "title":title, "desc":desc}
+                    them[folder].append(grouped)
+                    data["static/"+img] = grouped
 
     others   = them
-
+    f = open("static/bio.txt","r")
+    
+    curr_bio = ""
+    for line in f.readlines():
+        temp = line.strip()
+        if not temp:
+            pass
 @app.route("/")
 def index():
-    print(others)
-    return render_template("index.html",big=showcase[0],showcase=showcase[1:], others=others, order=order)
+    return render_template("index.html",big=showcase[0],showcase=showcase[1:], others=others,data=data)
 
 get_them()
 if __name__ == "__main__":
